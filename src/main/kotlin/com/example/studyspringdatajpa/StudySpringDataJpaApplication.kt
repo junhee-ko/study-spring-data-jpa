4e3748329d459858419d8b050c69f0c5c197ac56
junhee-ko <junheee.ko@gmail.com>
2023-10-08 13:26:03 +0900

    first commit

---
+++ src/main/kotlin/com/example/studyspringdatajpa/StudySpringDataJpaApplication.kt
@@ -0,0 +1,11 @@
+package com.example.studyspringdatajpa
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class StudySpringDataJpaApplication
+
+fun main(args: Array<String>) {
+    runApplication<StudySpringDataJpaApplication>(*args)
+}
