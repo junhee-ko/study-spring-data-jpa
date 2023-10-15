5e82f198a8bf8465adad09e1136c9cbc9ee2817a
junhee-ko <junheee.ko@gmail.com>
2023-10-15 21:57:28 +0900

    web > paging and sorting

--- src/main/kotlin/com/example/studyspringdatajpa/cotroller/MemberController.kt
+++ src/main/kotlin/com/example/studyspringdatajpa/cotroller/MemberController.kt
@@ -1,8 +1,12 @@
 package com.example.studyspringdatajpa.cotroller
 
+import com.example.studyspringdatajpa.dto.MemberDto
 import com.example.studyspringdatajpa.entity.Member
 import com.example.studyspringdatajpa.repository.MemberRepository
 import jakarta.annotation.PostConstruct
+import org.springframework.data.domain.Page
+import org.springframework.data.domain.Pageable
+import org.springframework.data.web.PageableDefault
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RestController
@@ -24,9 +28,26 @@ class MemberController(
         return member.username
     }
 
+    @GetMapping("/members")
+    fun findMembers(@PageableDefault(size = 5, sort = ["username"]) pageable: Pageable): Page<MemberDto> {
+        val page: Page<Member> = memberRepository.findAll(pageable)
+
+        val map: Page<MemberDto> = page.map {
+            MemberDto(
+                id = it.id,
+                username = it.username,
+                age = it.age
+            )
+        }
+
+        return map
+    }
+
     @PostConstruct
     fun init(){
-        val member = Member(username = "member1", age = 20, team = null)
-        memberRepository.save(member)
+        (1..100).forEach {
+            val member = Member(username = "username + $it", age = it, team = null)
+            memberRepository.save(member)
+        }
     }
 }
